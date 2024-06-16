35811515f1d2fff25d145b7f5a8678fa1823e8e8
Vishrut Jha <vishrutkmr7@icloud.com>
2024-06-15 20:16:35 -0500

    chore: Update Timeline component with technology badges

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -1,9 +1,40 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import { Button, Timeline, Card } from "flowbite-react";
-import { FaGraduationCap, FaBriefcase } from "react-icons/fa";
+import { Button, Timeline, Card, Badge } from "flowbite-react";
+import {
+  FaGraduationCap,
+  FaBriefcase,
+  FaPython,
+  FaSwift,
+  FaDatabase,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaGithub,
+  FaDocker,
+  FaCss3,
+  FaHtml5,
+  FaCode,
+} from "react-icons/fa";
+
+const techIconMap: { [key: string]: any } = {
+  Python: FaPython,
+  Swift: FaSwift,
+  SwiftUI: FaSwift,
+  SwiftData: FaSwift,
+  CoreML: FaSwift,
+  JavaScript: FaJs,
+  SQL: FaDatabase,
+  DB: FaDatabase,
+  React: FaReact,
+  NodeJS: FaNodeJs,
+  GitHub: FaGithub,
+  Git: FaGithub,
+  Docker: FaDocker,
+  CSS: FaCss3,
+  HTML: FaHtml5,
+};
 
 interface TimelineItem {
   time: string;
@@ -16,6 +47,7 @@ interface TimelineItem {
   type: "education" | "work";
   logo: string;
   color: string;
+  technologies: string[];
 }
 
 const JourneyTimeline: React.FC = () => {
@@ -28,35 +60,47 @@ const JourneyTimeline: React.FC = () => {
   }, []);
 
   return (
-    <Timeline>
-      {timelineData.map((item, index) => (
-        <Timeline.Item key={index}>
-          <Timeline.Point
-            icon={item.type === "education" ? FaGraduationCap : FaBriefcase}
-            className="ring-offset-0"
-          />
-          <Timeline.Content className="w-full">
-            <Card
-              className={`max-w-sm rounded-lg shadow-lg bg-${item.color}`}
-              horizontal
-              imgSrc={item.logo}
-              imgAlt={item.title}
-            >
-              <Timeline.Time>{item.time}</Timeline.Time>
-              <Timeline.Title className="text-white dark:text-white">
-                {item.title}
-              </Timeline.Title>
-              <Timeline.Body>{item.body}</Timeline.Body>
-              {item.link && (
-                <Button color="gray" href={item.link.url}>
-                  {item.link.text}
-                </Button>
-              )}
-            </Card>
-          </Timeline.Content>
-        </Timeline.Item>
-      ))}
-    </Timeline>
+    <div className="container mx-auto p-4">
+      <Timeline>
+        {timelineData.map((item, index) => (
+          <Timeline.Item key={index}>
+            <Timeline.Point
+              icon={item.type === "education" ? FaGraduationCap : FaBriefcase}
+              className="ring-offset-0"
+            />
+            <Timeline.Content className="w-full">
+              <Card
+                className={`bg-${item.color} dark:bg-${item.color} text-gray-300`}
+                horizontal
+                imgSrc={item.logo}
+                imgAlt={item.title}
+              >
+                <Timeline.Time>{item.time}</Timeline.Time>
+                <Timeline.Title className="text-gray-300 dark:text-white">
+                  {item.title}
+                </Timeline.Title>
+                <Timeline.Body>{item.body}</Timeline.Body>
+                {item.link && (
+                  <Button color="gray" href={item.link.url}>
+                    {item.link.text}
+                  </Button>
+                )}
+                <div className="mt-2 flex flex-wrap gap-2">
+                  {item.technologies.map((tech, techIndex) => {
+                    const Icon = techIconMap[tech] || FaCode;
+                    return (
+                      <Badge key={techIndex} icon={Icon}>
+                        {tech}
+                      </Badge>
+                    );
+                  })}
+                </div>
+              </Card>
+            </Timeline.Content>
+          </Timeline.Item>
+        ))}
+      </Timeline>
+    </div>
   );
 };
 
