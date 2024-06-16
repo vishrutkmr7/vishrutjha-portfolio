bc300e61db1ed0e087674e60cb79693dc41495bd
Vishrut Jha <vishrutkmr7@icloud.com>
2024-06-15 19:44:47 -0500

    card-ify

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Button, Timeline } from "flowbite-react";
-import { FaCalendar } from "react-icons/fa";
+import Image from "next/image";
+import { Button, Timeline, Card } from "flowbite-react";
+import { FaGraduationCap, FaBriefcase } from "react-icons/fa";
 
 interface TimelineItem {
   time: string;
@@ -12,6 +13,9 @@ interface TimelineItem {
     text: string;
     url: string;
   };
+  type: "education" | "work";
+  logo: string;
+  color: string;
 }
 
 const JourneyTimeline: React.FC = () => {
@@ -27,19 +31,28 @@ const JourneyTimeline: React.FC = () => {
     <Timeline>
       {timelineData.map((item, index) => (
         <Timeline.Item key={index}>
-          <Timeline.Point icon={FaCalendar} />
-          <Timeline.Content>
-            <Timeline.Time>{item.time}</Timeline.Time>
-            <Timeline.Title className="text-white dark:text-white">
-              {item.title}
-            </Timeline.Title>
-            <Timeline.Body>{item.body}</Timeline.Body>
-            {item.link && (
-              <Button color="gray" href={item.link.url}>
-                {item.link.text}
-                {/* <HiArrowNarrowRight className="ml-2 h-3 w-3" /> */}
-              </Button>
-            )}
+          <Timeline.Point
+            icon={item.type === "education" ? FaGraduationCap : FaBriefcase}
+            className="ring-offset-0"
+          />
+          <Timeline.Content className="w-full">
+            <Card
+              className={`max-w-sm rounded-lg shadow-lg bg-${item.color}`}
+              horizontal
+              imgSrc={item.logo}
+              imgAlt={item.title}
+            >
+              <Timeline.Time>{item.time}</Timeline.Time>
+              <Timeline.Title className="text-white dark:text-white">
+                {item.title}
+              </Timeline.Title>
+              <Timeline.Body>{item.body}</Timeline.Body>
+              {item.link && (
+                <Button color="gray" href={item.link.url}>
+                  {item.link.text}
+                </Button>
+              )}
+            </Card>
           </Timeline.Content>
         </Timeline.Item>
       ))}
