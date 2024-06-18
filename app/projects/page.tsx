50522c73c26faf93465c3ee2c13611eb589d9b61
Vishrut Jha <vishrutkmr7@icloud.com>
2024-06-17 22:43:47 -0500

    fix deploymet issues

---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProjectsPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-20">
+      <h1 className="text-3xl font-bold mb-4">What I have worked on.</h1>
+      <br />
+    </div>
+  );
+};
+
+export default ProjectsPage;
