e5499266c7f2958c2b080950fc80e2a725958ed3
Vishrut Jha <vishrutkmr7@icloud.com>
2024-06-22 22:56:58 -0500

    Update Header component with responsive layout and improved navigation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,39 +9,41 @@ const Header: React.FC = () => {
     <Navbar
       fluid
       rounded
-      className="bg-gray-900 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-600 flex items-center justify-center"
+      className="bg-gray-900 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-gray-600"
     >
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Navbar.Link
-          as={Link}
-          href="/"
-          className="block py-2 px-3 text-gray-300 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
-        >
-          Home
-        </Navbar.Link>
-        <Navbar.Link
-          as={Link}
-          href="/journey"
-          className="block py-2 px-3 text-gray-300 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
-        >
-          Journey
-        </Navbar.Link>
-        <Navbar.Link
-          as={Link}
-          href="/projects"
-          className="block py-2 px-3 text-gray-300 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
-        >
-          Projects
-        </Navbar.Link>
-        <Navbar.Link
-          as={Link}
-          href="/achievements"
-          className="block py-2 px-3 text-gray-300 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
-        >
-          Achievements
-        </Navbar.Link>
-      </Navbar.Collapse>
+      <div className="container mx-auto flex items-center justify-between w-full px-4">
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link
+            as={Link}
+            href="/"
+            className="block py-2 px-3 text-gray-300 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
+          >
+            Home
+          </Navbar.Link>
+          <Navbar.Link
+            as={Link}
+            href="/journey"
+            className="block py-2 px-3 text-gray-300 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
+          >
+            Journey
+          </Navbar.Link>
+          <Navbar.Link
+            as={Link}
+            href="/projects"
+            className="block py-2 px-3 text-gray-300 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
+          >
+            Projects
+          </Navbar.Link>
+          <Navbar.Link
+            as={Link}
+            href="/achievements"
+            className="block py-2 px-3 text-gray-300 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
+          >
+            Achievements
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </div>
     </Navbar>
   );
 };
