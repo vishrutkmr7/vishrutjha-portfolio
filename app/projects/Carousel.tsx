cfb46eb6b53b390c9e5e8091f055e759c8254ee0
Vishrut Jha <vishrutkmr7@icloud.com>
2024-06-27 01:14:35 -0500

    chore: Add react-swipeable dependency and implement swipe functionality in ProjectsCarousel

--- app/projects/Carousel.tsx
+++ app/projects/Carousel.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import Image from "next/image";
 import { Card, Button, Badge, Spinner } from "flowbite-react";
 import {
   FaCode,
   FaPython,
   FaSwift,
-  FaGitAlt,
+  FaGithub,
   FaDatabase,
   FaHtml5,
   FaCss3Alt,
   FaJs,
   FaReact,
+  FaAngular,
+  FaAppStoreIos,
+  FaPodcast,
+  FaMobile,
+  FaLaptop,
+  FaTablet,
 } from "react-icons/fa";
+import { useSwipeable } from "react-swipeable";
 
 interface projectItem {
   title: string;
@@ -29,8 +37,11 @@ interface projectItem {
 const techIconMap: { [key: string]: any } = {
   Python: FaPython,
   Swift: FaSwift,
-  iOS: FaSwift,
-  Git: FaGitAlt,
+  SwiftUI: FaSwift,
+  iOS: FaMobile,
+  macOS: FaLaptop,
+  iPadOS: FaTablet,
+  Git: FaGithub,
   Database: FaDatabase,
   SQL: FaDatabase,
   Redis: FaDatabase,
@@ -40,11 +51,13 @@ const techIconMap: { [key: string]: any } = {
   JavaScript: FaJs,
   TypeScript: FaJs,
   React: FaReact,
+  Angular: FaAngular,
 };
 
 const ProjectsCarousel: React.FC = () => {
   const [projects, setProjects] = useState<projectItem[]>([]);
   const [loading, setLoading] = useState(true);
+  const carouselRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     fetch("/data/projectsData.json")
@@ -56,6 +69,21 @@ const ProjectsCarousel: React.FC = () => {
       .catch(() => setLoading(false)); // Ensure loading is stopped even if there is an error
   }, []);
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      if (carouselRef.current) {
+        carouselRef.current.scrollBy({ left: 300, behavior: "smooth" });
+      }
+    },
+    onSwipedRight: () => {
+      if (carouselRef.current) {
+        carouselRef.current.scrollBy({ left: -300, behavior: "smooth" });
+      }
+    },
+    // preventDefaultTouchmoveEvent: false,
+    trackMouse: true,
+  });
+
   return (
     <div className="container mx-auto p-4 my-8">
       <h1 className="text-4xl sm:text-5xl font-bold mb-8 mt-4">
@@ -66,11 +94,24 @@ const ProjectsCarousel: React.FC = () => {
           <Spinner aria-label="Cookin' up some projects!" />
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div
+          {...handlers}
+          ref={carouselRef}
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 overflow-x-auto"
+        >
           {projects.map((project, index) => (
             <Card
               key={index}
-              imgSrc={project.image}
+              renderImage={() => (
+                <Image
+                  width={500}
+                  height={500}
+                  loading="lazy"
+                  src={`/${project.image}`}
+                  alt={project.title}
+                  className="w-full rounded-lg h-full object-cover object-center"
+                />
+              )}
               className={`bg-grey-600 dark:bg-grey-900 text-gray-300 p-4 rounded-lg border border-gray-600`}
             >
               <h2 className="text-xl font-bold tracking-tight text-gray-300 dark:text-white">
@@ -83,7 +124,17 @@ const ProjectsCarousel: React.FC = () => {
                   href={project.link.url}
                   target="_blank"
                   rel="noopener noreferrer"
+                  className="flex items-center space-x-2"
                 >
+                  {project.link.text === "Source Code" && (
+                    <FaGithub className="mr-2 h-5 w-5" />
+                  )}
+                  {project.link.text === "App Store" && (
+                    <FaAppStoreIos className="mr-2 h-5 w-5" />
+                  )}
+                  {project.link.text === "Listen" && (
+                    <FaPodcast className="mr-2 h-5 w-5" />
+                  )}
                   {project.link.text}
                 </Button>
               )}
