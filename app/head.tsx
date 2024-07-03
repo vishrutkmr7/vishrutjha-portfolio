a0d5956e3ce29ba484bc9ce0d4d5bcd6f50a8da6
Vishrut Jha <vishrutkmr7@icloud.com>
2024-07-03 12:44:31 -0500

    chore: Add Google Analytics tracking code

--- app/head.tsx
+++ app/head.tsx
@@ -37,6 +37,20 @@ export default function Head() {
         `}
       </Script>
       {/* End Google Tag Manager */}
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-EM96FL2J50"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EM96FL2J50');
+        `}
+      </Script>
+      {/* End Google Analytics */}
     </>
   );
 }
