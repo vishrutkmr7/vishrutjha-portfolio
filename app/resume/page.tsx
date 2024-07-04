346f27b24fd73e68a1c4ca3771eff03c1dc8c909
Vishrut Jha <vishrutkmr7@icloud.com>
2024-07-04 17:31:51 -0500

    chore: Update resume page metadata and image alt text

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,13 +2,13 @@ import { Metadata } from "next";
 import { DOMAIN } from "@/app/constants";
 
 export const metadata: Metadata = {
-  title: "Vishrut Jha - Resume",
+  title: "Résumé | Vishrut Jha",
   description:
-    "Professional resume of Vishrut Jha - Full Stack and iOS Developer",
+    "Professional Résumé of Vishrut Jha - Full Stack and iOS Developer",
   openGraph: {
-    title: "Vishrut Jha - Resume",
+    title: "Résumé | Vishrut Jha",
     description:
-      "Professional resume of Vishrut Jha - Full Stack and iOS Developer",
+      "Professional Résumé of Vishrut Jha - Full Stack and iOS Developer",
     url: `${DOMAIN}/resume`,
     siteName: "Vishrut Jha Portfolio",
     images: [
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
         url: `${DOMAIN}/resume-preview.png`,
         width: 1336,
         height: 1732,
-        alt: "Vishrut Jha Resume Preview",
+        alt: "Vishrut Jha Résumé Preview",
       },
     ],
     locale: "en_US",
@@ -24,9 +24,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Vishrut Jha - Resume",
+    title: "Résumé | Vishrut Jha",
     description:
-      "Professional resume of Vishrut Jha - Full Stack and iOS Developer",
+      "Professional Résumé of Vishrut Jha - Full Stack and iOS Developer",
     creator: "@vishrutkmr7",
     images: [`${DOMAIN}/resume-preview.png`],
   },
