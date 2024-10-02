f87fc6cc3e67a7f56d388bc84e9423ee6e7a2cc1
Vishrut Jha <me@vishrutjha.com>
2024-10-02 03:51:18 -0700

    feat(redirects): add permanent redirect from /hey to https://nohello.net in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/hey",
+        destination: "https://nohello.net",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
