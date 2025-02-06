712ead35e54c668da3f6dfa1dabc9be228ab547f
Vishrut Jha <vishrut@pricklypear.io>
2025-02-05 18:20:59 -0700

    seo fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,7 +91,7 @@ export const metadata: Metadata = {
     google: 'YOUR-GOOGLE-SITE-VERIFICATION',
     yandex: 'YOUR-YANDEX-VERIFICATION',
     other: {
-      me: ['mailto:your-email@vishrutjha.com', 'https://github.com/vishrutkmr7'],
+      me: ['mailto:me@vishrutjha.com', 'https://github.com/vishrutkmr7'],
     },
   },
   openGraph: {
