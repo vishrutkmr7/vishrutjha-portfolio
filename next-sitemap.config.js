ad149db11b1246bdbeee098b74a29302b0a0f225
Vishrut Jha <vishrut@pricklypear.io>
2025-02-23 00:02:32 -0700

    SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,6 +6,18 @@ module.exports = {
   changefreq: 'weekly',
   priority: 0.7,
   exclude: ['/api/*', '/server-sitemap.xml'],
+  // Add additional paths to be included
+  additionalPaths: async config => {
+    const result = [];
+    result.push({
+      loc: '/llms.txt',
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date().toISOString(),
+    });
+
+    return result;
+  },
   robotsTxtOptions: {
     policies: [
       {
